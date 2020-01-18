bdb7d5f00921b21ec0e96ee6c3f8673de7a6a2fb
AckiySatana <ter.on@mail.ru>
2020-01-18 11:16:21 +0300

    0. Map, HashMap [#212652]

---
+++ Chapter_003/src/main/java/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("coca_cola@yandex.ru", "Газировкин Лимонад Сиропчикович");
+        map.put("barankin@yandex.ru", "Бубликов Пончик Кексович");
+        for (String key: map.keySet()) {
+            System.out.println(key + map.get(key));
+        }
+    }
+}
