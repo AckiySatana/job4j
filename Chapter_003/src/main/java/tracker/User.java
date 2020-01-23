bbfa1f37c1c5849f61aa594a41f7fcffb32b80ad
AckiySatana <ter.on@mail.ru>
2020-01-23 15:16:41 +0300

    0. Сортировка

---
+++ Chapter_003/src/main/java/tracker/User.java
@@ -0,0 +1,35 @@
+package tracker;
+
+import java.util.List;
+
+/**
+ Необходимо создать модель User с полями name, age.
+
+ Класс User должен реализовать интерфейс Comparable.
+ */
+
+public class User implements Comparable<User> {
+    String name;
+    int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return Integer.compare(this.age, o.age);
+    }
+}
